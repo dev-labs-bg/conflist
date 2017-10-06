011ec1c852abe1b648ea7ba7f8d306430f75fea7
superKalo <superkalo@devlabs.bg>
2017-10-06 12:03:02 +0300

    Add: notify plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const notify = require('gulp-notify');
 
 gulp.task('styles', function () {
   return gulp.src('./sass/style.scss')
@@ -9,9 +10,12 @@ gulp.task('styles', function () {
         browsers: ['last 2 versions'],
         cascade: false
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(notify('Styles compiled!'));
 });
 
 gulp.task('styles:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['styles'], function(){
+      return gulp.pipe(notify('Styles compiled!'));
+  });
 });
