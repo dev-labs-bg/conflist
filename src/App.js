e5bd3eedcaf22c582a5735602c85b8540e21f084
gergana95 <geriss1995@gmail.com>
2017-12-11 10:05:02 +0200

    Add: footer in common components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './common/header/Header';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Footer />
       </div>
     );
   }
