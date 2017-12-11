cef2aa403763e33f588586eb9e90b7ff7edcd36b
gergana95 <geriss1995@gmail.com>
2017-12-11 17:25:11 +0200

    Add: import css stylesheet|

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './assets/css/style.css'
+import './assets/css/style.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
