88bd8e464ccae9a41443d6f2b337fd19dca8aa89
gergana95 <geriss1995@gmail.com>
2017-12-11 15:21:31 +0200

    fix: eslint warnings

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Header from './common/Header';
 import Footer from './common/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const footer = () => {
-    return (
+const footer = () => (
     <footer className="footer d-flex align-content-center">
-        <span className="footer__text my-auto mx-auto font-weight-normal">© 2017 conf<span className="font-weight-bold">list</span>. All rights reserved.
+        <span
+            className="footer__text my-auto mx-auto font-weight-normal"
+        >
+            © 2017 conf<span className="font-weight-bold">list</span>
+            . All rights reserved.
         </span>
     </footer>
-    );
-};
+);
 
 export default footer;
