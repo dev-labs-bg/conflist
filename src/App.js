e8038f742d0b288bf58eae4e200e15d76aa79394
gergana95 <geriss1995@gmail.com>
2017-12-12 11:02:47 +0200

    Add: import cardlist in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './common/Header';
 import Footer from './common/Footer';
+import CardList from './common/CardList/CardList';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Header />
+                <CardList />
                 <Footer />
             </div>
         );
