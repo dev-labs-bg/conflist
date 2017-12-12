3497e6ef447b50f183fa2cbc575a96076e2ff73b
gergana95 <geriss1995@gmail.com>
2017-12-12 11:01:08 +0200

    Add: cardlist component

---
+++ src/common/CardList/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Card from '../Card';
+
+const cardList = () => (
+    <div className="container mx-auto pt-5 pb-5">
+        <div className="mb-5">
+            <h2 className="cards-date font-weight-normal">June</h2>
+            <Card />
+            <Card />
+        </div>
+    </div>
+);
+
+export default cardList;
