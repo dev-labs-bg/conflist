eb5f23d4eb7da6c670fefedbb9cc2601d2bc7d14
gergana95 <geriss1995@gmail.com>
2017-12-12 11:30:33 +0200

    Add: homepage component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './common/Header';
-import Footer from './common/Footer';
-import CardList from './common/CardList/CardList';
+import HomePage from './HomePage';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header />
-                <CardList />
-                <Footer />
+                <HomePage />
             </div>
         );
     }
---
+++ src/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import CardList from '../common/CardList/CardList';
+
+const homePage = () => (
+    <div>
+        <Header />
+        <CardList />
+        <Footer />
+    </div>
+);
+
+
+export default homePage;
