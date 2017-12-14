8b8af244064ad0c30f6118b0163a9c653017bb26
Gergana Mihaylova <geriss1995@gmail.com>
2017-12-14 14:57:29 +0200

    Add: calendar svg in images

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import smashing from '../assets/images/smashing-conf.png';
+import calendar from '../assets/images/callendar.svg';
 
 const card = () => (
     <div className="card mb-2">
@@ -16,110 +17,7 @@ const card = () => (
             <span className="d-flex justify-content-between">
 
                 <div className="card__info">
-                    <svg
-                        className="mr-1"
-                        width="19"
-                        height="19"
-                        viewBox="0 0 19 19"
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlnsXlink="http://www.w3.org/1999/xlink"
-                    >
-                        <title>Small-calendar svg</title>
-                        <desc>Small calendar logo</desc>
-                        <g
-                            id="Page-1"
-                            stroke="none"
-                            strokeWidth="1"
-                            fill="none"
-                            fillRule="evenodd"
-                        >
-                            <g
-                                id="conflist-home-page-logged-in"
-                                transform="translate(-572.000000, -677.000000)"
-                                fillRule="nonzero"
-                                fill="#F2706D"
-                            >
-                                <g
-                                    id="conf-card"
-                                    transform="translate(309.000000, 639.000000)"
-                                >
-                                    <g
-                                        id="small-calendar-copy"
-                                        transform="translate(263.000000, 38.000000)"
-                                    >
-                                        <path
-                                            d="M17.4170625,1.78125 L14.84375,1.78125 L14.84375,0.59375 C14.84375,0.26540625 14.5783438,0 14.25,0 C13.9216562,0 13.65625,0.26540625 13.65625,0.59375 L13.65625,1.78125 L10.09375,1.78125 L10.09375,0.59375 C10.09375,0.26540625 9.82775,0 9.5,0 C9.17225,0 8.90625,0.26540625 8.90625,0.59375 L8.90625,1.78125 L5.34375,1.78125 L5.34375,0.59375 C5.34375,0.26540625 5.07775,0 4.75,0 C4.42225,0 4.15625,0.26540625 4.15625,0.59375 L4.15625,1.78125 L1.58353125,1.78125 C0.7089375,1.78125 0,2.48959375 0,3.3641875 L0,17.4164687 C0,18.2910625 0.7089375,19 1.58353125,19 L17.4170625,19 C18.2916562,19 19,18.2910625 19,17.4164687 L19,3.3641875 C19,2.48959375 18.2916562,1.78125 17.4170625,1.78125 Z M17.8125,17.4164687 C17.8125,17.6349687 17.6349687,17.8125 17.4170625,17.8125 L1.58353125,17.8125 C1.36503125,17.8125 1.1875,17.6349687 1.1875,17.4164687 L1.1875,3.3641875 C1.1875,3.14628125 1.36503125,2.96875 1.58353125,2.96875 L4.15625,2.96875 L4.15625,4.15625 C4.15625,4.48459375 4.42225,4.75 4.75,4.75 C5.07775,4.75 5.34375,4.48459375 5.34375,4.15625 L5.34375,2.96875 L8.90625,2.96875 L8.90625,4.15625 C8.90625,4.48459375 9.17225,4.75 9.5,4.75 C9.82775,4.75 10.09375,4.48459375 10.09375,4.15625 L10.09375,2.96875 L13.65625,2.96875 L13.65625,4.15625 C13.65625,4.48459375 13.9216562,4.75 14.25,4.75 C14.5783438,4.75 14.84375,4.48459375 14.84375,4.15625 L14.84375,2.96875 L17.4170625,2.96875 C17.6349687,2.96875 17.8125,3.14628125 17.8125,3.3641875 L17.8125,17.4164687 Z"
-                                            id="Shape"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="4.15625"
-                                            y="7.125"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="4.15625"
-                                            y="10.09375"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="4.15625"
-                                            y="13.0625"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="8.3125"
-                                            y="13.0625"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="8.3125"
-                                            y="10.09375"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="8.3125"
-                                            y="7.125"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="12.46875"
-                                            y="13.0625"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="12.46875"
-                                            y="10.09375"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                        <rect
-                                            id="Rectangle-path"
-                                            x="12.46875"
-                                            y="7.125"
-                                            width="2.375"
-                                            height="1.78125"
-                                        />
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </svg>
+                    <img src={calendar} className="mr-1" alt="small calendar" />
                     <span className="card__dates">12nd&#45;14th June, 2017
                         <span className="text-info"> | </span> New York, USA
                     </span>
