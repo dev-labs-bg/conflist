d05d8afa536ac3a31e136e3f6db821bea0393ac9
Gergana Mihaylova <geriss1995@gmail.com>
2017-12-14 18:21:47 +0200

    Add: new events

--- src/EventsHandling/Events.js
+++ src/EventsHandling/Events.js
@@ -1,7 +1,7 @@
-import './Event';
+import Event from './Event';
 
-new Event({
-    imageCard: 'smashing-conf.pdf',
+const smashingConf = new Event({
+    imageCard: 'smashing-conf.png',
     dates: {
         start: '12th June',
         end: '14th June',
@@ -15,5 +15,59 @@ new Event({
         'javascript',
         'css',
         'web',
-    ]
+    ],
+});
+
+const cssDay = new Event({
+    imageCard: 'css-day.png',
+    dates: {
+        start: '15th June',
+        end: '16th June',
+    },
+    location: 'New York, USA',
+    wishListed: 56,
+    title: 'CSSDay 2017',
+    description:
+        'CSS Day 2016 was a two-day advanced web development conference, one day about CSS, and one day about HTML. We &#39;ll be back.',
+    tags: [
+        'javascript',
+        'css',
+        'web',
+    ],
+});
+
+const angular = new Event({
+    imageCard: 'angular-photo.png',
+    dates: {
+        start: '6th July',
+        end: '7th July',
+    },
+    location: 'Barcelona,Spain',
+    wishListed: 312,
+    title: 'AngularCamp',
+    description:
+        'AngularCamp is a collaborative community event for all people which are interested in coming together to share their recent work.',
+    tags: [
+        'javascript',
+        'css',
+        'web',
+    ],
+});
+
+const frontend = new Event({
+    imageCard: 'front-logo.png',
+    dates: {
+        start: '31st Aug',
+        end: '1st Sep',
+    },
+    location: 'Barcelona,Spain',
+    wishListed: 89,
+    title: 'Frontend Conference Zurich',
+    description:
+        'AngularCamp is a collaborative community event for all people which are interested in coming together to share their recent work.',
+    tags: [
+        'javascript',
+        'css',
+        'web',
+    ],
 });
