f2e6a1808b48002cb91d819ddc17025fc26259ef
superKalo <superkalo@devlabs.bg>
2017-12-14 17:50:30 +0200

    AddL finallize Event structure and set default values

--- src/Event.js
+++ src/Event.js
@@ -1,5 +1,16 @@
 class Event {
-    constructor(event) {
+    constructor({
+        imageCard = null,
+        dates = {
+            start: null,
+            end: null,
+        },
+        location = '',
+        wishListed = null,
+        title = '',
+        description = '',
+        tags = [],
+    }) {
         this.imageCard = imageCard;
         this.dates = dates;
         this.location = location;
@@ -8,25 +19,24 @@ class Event {
         this.description = description;
         this.tags = tags;
     }
-
-    const event = {
-        imageCard: imageCard,
-        dates: {
-            start: '12th June',
-            end: '14th June',
-        },
-
-        location: 'New York, USA',
-        wishListed: 18,
-        title: 'SmashingConf New York 2017',
-        description:
-            'With 1 track, 2 full conference days, 10 workshops, 15 excellent speakers and just 400 available seats - tickets will go fast!',
-        tags: [
-            'javascript',
-            'css',
-            'web',
-        ]
-    }
 }
 
 export default Event;
+
+// new Event({
+//     imageCard: imageCard,
+//     dates: {
+//         start: '12th June',
+//         end: '14th June',
+//     },
+//     location: 'New York, USA',
+//     wishListed: 18,
+//     title: 'SmashingConf New York 2017',
+//     description:
+//         'With 1 track, 2 full conference days, 10 workshops, 15 excellent speakers and just 400 available seats - tickets will go fast!',
+//     tags: [
+//         'javascript',
+//         'css',
+//         'web',
+//     ]
+// });
