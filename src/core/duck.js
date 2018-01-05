b33b73562088796ea39baface35a2fef26a7e9a2
Gergana Mihaylova <geriss1995@gmail.com>
2018-01-05 18:01:03 +0200

    Add: duck.js reducers

--- src/core/duck.js
+++ src/core/duck.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 
 // Actions
 const FETCH_CONFERENCES = 'conflist/src/core/duck/FETCH_CONFERENCES';
+const SET_CONFERENCEES = 'SET_CONFERENCEES';
 
 // Action Creators
+export function setConferences( conferences ) => {
+    return {
+        type: SET_CONFERENCEES,
+        conferences: conferences
+    };
+};
+
 export function fetchConferences(state, action) {
     return dispatch => {
         axios.get('https://api.conflist.devlabs-projects.com/api/v1/conferences')
             .then(response => {
-                console.log(response);
+                dispatch(setConferences(response));
             })
             .catch(response => {
                 console.log('error');
@@ -21,6 +29,11 @@ export default function reducer(state = {}, action) {
     switch (action.type) {
     case FETCH_CONFERENCES:
         return fetchConferences(state, action);
+    case SET_CONFERENCEES:
+        return {
+            ...state,
+            conferences: state.conferences
+        }
     default: return state;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './core/duck';
 
@@ -14,5 +15,5 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
