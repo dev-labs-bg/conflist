84773e452a4d7b0d4cc6926496ebbfd148af6e91
Gergana Mihaylova <geriss1995@gmail.com>
2018-01-09 12:09:59 +0200

    Add: proptypes and renderCards

--- src/EventsHandling/CardList/CardList.js
+++ src/EventsHandling/CardList/CardList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Card from '../../common/Card';
-
+import Event from '../Event';
 
 class CardList extends Component {
 
@@ -9,15 +10,21 @@ class CardList extends Component {
         const renderCards = this.props.events.map(el => {
             return <Card event={el} />;
         });
+
         return (
             <div className="container mx-auto pt-5 pb-5">
                 <div className="mb-5">
                     <h2 className="cards-date font-weight-normal">June</h2>
-                    {renderCards}
+                    {this.props.events ? renderCards : null}
                 </div>
             </div>
         );
     }
+}
+CardList.propTypes = {
+    events: PropTypes.arrayOf(PropTypes.instanceOf(Event)),
 };
-
+CardList.defaultProps = {
+    events: [],
+};
 export default CardList;
