8253d8aa95ceae8677e8c8c30d175c08b4b64978
superKalo <superkalo@devlabs.bg>
2018-01-09 16:44:26 +0200

    Add: group events by month

--- src/EventsHandling/CardList/CardList.js
+++ src/EventsHandling/CardList/CardList.js
@@ -5,33 +5,54 @@ import moment from 'moment';
 import Card from '../../common/Card';
 import Event from '../Event';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class CardList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.eventsGroupedByMonth = {};
+
+        // TODO: Delete me :) Just for the test.
+        // this.props.events.map( (event, i) => {
+        //     event.start = '2017-0' + (5 + i) + '-11';
+        //
+        //     return event;
+        // });
+
+        this.props.events.forEach((event) => {
+            const month = moment(event.start).format('MMMM|YYYY');
+
+            this.eventsGroupedByMonth[month] = {
+                month: moment(event.start).format('MMMM'),
+                data: this.eventsGroupedByMonth[month] ?
+                    [...this.eventsGroupedByMonth[month].data, event] : [event],
+            };
+        });
+    }
+
+    // TODO: Handle multiple events per month!
+    renderCards = () => {
+        const { events } = this.props;
+
+        if (!events.length) {
+            return null;
+        }
+
+        return events.map(event => (
+            <div className="mb-5">
+                <h2 className="cards-date font-weight-normal">
+                    {moment(event.start).format('MMMM')}
+                </h2>
+                <Card event={event} />
+            </div>
+        ));
+    }
+
     render() {
-        let month;
-        let groupedEvents = [];
-        const dates = this.props.events.forEach(el => {
-            month = moment(el.start).format('MMMM');
-            groupedEvents.push({
-                month, el,
-            });
-        });
-        console.log(groupedEvents);
-
-        const renderCards = this.props.events.map(el => {
-            return (
-                <div className="mb-5">
-                    <h2 className="cards-date font-weight-normal">
-                        {moment(el.start).format('MMMM')}
-                    </h2>
-                    <Card event={el} />
-                </div>
-            );
-        });
-
         return (
             <div className="container mx-auto pt-5 pb-5">
                 <div className="mb-5">
-                    {this.props.events ? renderCards : null}
+                    { this.renderCards() }
                 </div>
             </div>
         );
