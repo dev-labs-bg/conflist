883814f73a757f5eec841723a0a659626e783df1
Gergana Mihaylova <geriss1995@gmail.com>
2018-01-10 17:57:19 +0200

    TODO: get event data from api call

--- src/InsidePage/InsidePage.js
+++ src/InsidePage/InsidePage.js
@@ -1,11 +1,96 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const InsidePage = () => {
+import { getFormattedDate } from '../core/Dates';
+
+const InsidePage = (props) => {
+    console.log(props);
+    console.log(props.location.pathname);
+    const eventId = props.location.pathname.substring(7);
+    console.log(eventId);
+    const events = props.conference.data;
+    console.log(events);
+
+    let event;
+    props.conference.data.forEach( (ev) => {
+        ev.id.indexOf(eventId) ? null : event = { ...ev };
+    });
+    console.log(event);
+    console.log(event.pictureUrl);
+    
+    const renderSpeakers = () => {
+        const renderImages = [];
+        event.speakers.map((speaker) => {
+            renderImages.push(<img
+                key={speaker.id}
+                src={speaker.pictureUrl}
+                width="40"
+                height="40"
+                alt={speaker.name}
+            />);
+        });
+        return renderImages;
+    };
+
     return (
         <div className="container__register mx-auto pt-5 pb-5 d-flex flex-column">
-            <img className="mx-auto mb-5" src="images/supercat-inside.png" width="381" height="388" alt="conference logo" />
+            <img
+                className="mx-auto mb-5"
+                src={event.pictureUrl}
+                width="381"
+                height="388"
+                alt={event.name}
+            />
+
+            <div className="mb-1">
+                <h4 className="mb-2">{event.name}</h4>
+                <span className="card__dates mb-2">{getFormattedDate(event.start, event.end)}
+                    <span className="text-info"> | </span> {event.venue}, {event.city}, {event.country}
+                </span>
+
+                <div className="mb-3">
+                    <span className="badge badge-pill badge-light mr-2">javascript</span>
+                    <span className="badge badge-pill badge-light mr-2">css</span>
+                    <span className="badge badge-pill badge-light">web</span>
+                </div>
+
+                <div className="mb-4 text-bottom d-flex">
+                    <svg className="mr-1" width="20px" height="19px" viewBox="0 0 20 19" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                        <defs />
+                        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                            <g id="conflist-home-page-logged-in" transform="translate(-986.000000, -677.000000)">
+                                <g id="conf-card" transform="translate(309.000000, 639.000000)">
+                                    <g id="ic_favorite_black_24px-(1)" transform="translate(675.000000, 35.000000)">
+                                        <polygon id="Shape" points="0 0 24 0 24 24 0 24" />
+                                        <path d="M12,21.35 L10.55,20.03 C5.4,15.36 2,12.28 2,8.5 C2,5.42 4.42,3 7.5,3 C9.24,3 10.91,3.81 12,5.09 C13.09,3.81 14.76,3 16.5,3 C19.58,3 22,5.42 22,8.5 C22,12.28 18.6,15.36 13.45,20.04 L12,21.35 Z" id="Shape" fill="#F2706D" fillRule="nonzero" />
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                    <h5 className="font-weight-normal d-inline">Going: <span className="text-secondary">{event.atendees}</span></h5>
+                </div>
+                {event.description}
+
+            </div>
+
+            <div className="mb-5">
+                <h5 className="font-weight-bold mb-2">Speakers:</h5>
+                {renderSpeakers()}
+            </div>
+
+            <div className="text-center">
+                <button className="btn btn-primary mr-5">Wanna go</button>
+                <button className="btn btn-secondary">Go to website</button>
+            </div>
         </div>
     );
 };
 
-export default InsidePage;
+const mapStateToProps = ({ conference }) => {
+    return {
+        conference,
+    };
+};
+
+export default connect(mapStateToProps)(InsidePage);
