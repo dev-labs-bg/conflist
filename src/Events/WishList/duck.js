9183fe4083f023d09123a80247affaafd7822972
Gergana Mihaylova <geriss1995@gmail.com>
2018-01-23 18:11:00 +0200

    Add: WishList component with reducer and Attend componen

--- src/Events/WishList/duck.js
+++ src/Events/WishList/duck.js
@@ -1,25 +1,78 @@
-// Actions
-const SET = 'wishList/SET';
+import API from '../../core/Api';
 
+// Actions
+const REQUEST = 'wishList/REQUEST';
+const SET = 'wishList/SET';
+const FAIL = 'wishList/FAIL';
+const DELETE = 'wishList/DELETE';
+
+const initialState = {
+    lastFetched: null,
+    isFetching: null,
+    data: [],
+    error: null,
+};
 
 // Reducer
-export default function reducer(state = {}, action = {}) {
+export default function reducer(state = initialState, action = {}) {
     switch (action.type) {
-    case SET:
-        const events = [];
-        events.push(action.event);
+    case REQUEST:
         return {
             ...state,
-            event: events,
+            isFetching: true,
+        };
+    case SET:
+        return {
+            ...state,
+            isFetching: false,
+            lastFetched: new Date().valueOf(),
+            data: [
+                ...state.data,
+                action.id,
+            ],
+        };
+    case FAIL:
+        return {
+            ...state,
+            isFetching: false,
+            error: action.error,
         };
     default: return state;
     }
 }
 
 // Action Creaters
-export function setWishListEvent(event) {
+export function requestWishListEvent() {
     return {
         type: SET,
-        event,
+    };
+}
+
+export function setWishListEvent(id) {
+    return {
+        type: SET,
+        id,
+    };
+}
+
+export function setWishListEventFail(error) {
+    return {
+        type: FAIL,
+        error,
+    };
+}
+
+
+export function attendConference() {
+    return dispatch => {
+        dispatch(requestWishListEvent());
+        API.attendConference()
+            .then(response => {
+                console.log(response);
+                dispatch(setWishListEvent(response.data));
+            })
+            .catch(error => {
+                dispatch(setWishListEventFail(error.response.data.message));
+            });
     };
 }
--- src/Events/WishList/index.js
+++ src/Events/WishList/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class WishList extends Component {
+    render() {
+        return (
+            <div>
 
-};
+            </div>
+        );
+    }
+}
 
-export default WishList;
+const mapStateToProps = ({ wishList }) => {
+    return {
+        wishList,
+    };
+}
+
+export default connect(mapStateToProps)(WishList);
