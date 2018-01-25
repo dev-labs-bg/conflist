af1ade3b7c25e07f17d4ac53687027828e97cca7
Gergana Mihaylova <geriss1995@gmail.com>
2018-01-25 15:38:08 +0200

    Add: heart svg as components

---
+++ src/common/HeartIcon.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const heartIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            width="20"
+            height="19"
+            viewBox="0 0 20 19"
+        >
+            <defs>
+                <path id="87eya" d="M1006 894.5c0 3.78-3.4 6.86-8.55 11.53l-1.45 1.32-1.45-1.31c-5.15-4.68-8.55-7.76-8.55-11.54 0-3.08 2.42-5.5 5.5-5.5 1.74 0 3.41.81 4.5 2.09a5.99 5.99 0 0 1 4.5-2.09c3.08 0 5.5 2.42 5.5 5.5zm-2 0c0-2-1.5-3.5-3.5-3.5-1.54 0-3.04.99-3.56 2.36h-1.87a3.9 3.9 0 0 0-3.57-2.36c-2 0-3.5 1.5-3.5 3.5 0 2.89 3.14 5.74 7.9 10.05l.1.1.1-.1c4.76-4.31 7.9-7.16 7.9-10.05z" />
+            </defs>
+            <g>
+                <g transform="translate(-986 -889)">
+                    <use
+                        fill="#f2706d"
+                        xlinkHref="#87eya"
+                    />
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export default heartIcon;
