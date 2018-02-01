5c7c5ee992ccfb258df2d02c5ea47f0373ab033e
Gergana Mihaylova <geriss1995@gmail.com>
2018-02-01 13:25:35 +0200

    Change: user class

--- src/ProfileSettings/User.js
+++ src/ProfileSettings/User.js
@@ -1,20 +1,14 @@
 class User {
     constructor({
         _id = null,
-        accessToken = '',
         profileImg = '',
-        url = '',
         name = '',
         email = '',
-        twitterId = null,
     }) {
         this.id = _id;
-        this.accessToken = accessToken;
         this.profileImg = profileImg;
-        this.url = url;
         this.name = name;
         this.email = email;
-        this.twitterId = twitterId;
     }
 }
 
