c0bcb509525dabe09301d1650ff8d9ecfc84ff4f
Gergana Mihaylova <geriss1995@gmail.com>
2018-02-26 17:27:45 +0200

    Add: position absolute

--- src/SuggestConference/index.js
+++ src/SuggestConference/index.js
@@ -6,16 +6,29 @@ class SuggestConference extends Component {
     }
 
     render() {
+        const style = {
+            paddingBottom: '85%',
+            height: '0',
+            width: '100%',
+            position: 'relative',
+        };
+        const iFrame = {
+            top: 0,
+            left: 0,
+            position: 'absolute',
+        };
         return (
             <div
-                className="w-100 h-100 embed-responsive embed-responsive-1by1"
+                style={style}
             >
                 <iframe
-                    className="embed-responsive-item"
+                    className="position-absolute"
                     title="Conflist Suggest a conference"
                     width="100%"
                     height="100%"
                     frameBorder="0"
+                    style={iFrame}
+                    allowfullscreen
                     src="https://devlabs.typeform.com/to/UJp2uM"
                 />
             </div>
