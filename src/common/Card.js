dfdae469c1fd6d13f73c4db37d8d383fd36e20c7
gergana95 <geriss1995@gmail.com>
2018-03-01 15:59:22 +0200

    Add: code documentation

--- src/common/Card.js
+++ src/common/Card.js
@@ -27,7 +27,9 @@ class Card extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            // Attend component
             isActive: this.props.wishListed,
+            // Modal component
             isOpen: this.props.wishListed,
         };
     }
