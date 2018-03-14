17498dc0e47c99e8963dacd63f41b43f1865f7b8
Gergana Mihaylova <geriss1995@gmail.com>
2018-03-14 12:51:08 +0200

    Add: page not found icon and heading

--- src/common/PageNotFound.js
+++ src/common/PageNotFound.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import eventIcon from '../assets/images/event-icon.svg';
+
 const pageNotFound = () => (
-    <div>
-        <h2 className="pt-5 text-center">Page Not Found!</h2>
+    <div className="pt-5 text-center pb-5">
+        <img className="mb-4" src={eventIcon} alt="no events" />
+        <h2 className="">Page not found!</h2>
+        <h4>The requested page is not available</h4>
     </div>
 );
 
