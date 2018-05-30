cb935eeda90db42790d7bce8e33568a08dde4ec7
gergana95 <gergana@devlabs.bg>
2018-05-30 10:25:11 +0300

    Add: prev label and background blue color to card section

--- src/Events/CalendarList/index.js
+++ src/Events/CalendarList/index.js
@@ -26,6 +26,9 @@ class CalendarList extends Component {
                             value={this.state.date}
                             locale="en-EN"
                             onClickDay={this.clickedDaYListener}
+                            prevLabel={
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="12" height="22" viewBox="0 0 12 22"><defs><path id="tdnaa" d="M159.57 694.98l10.22-9.92a.6.6 0 0 0 0-.88.65.65 0 0 0-.9 0l-10.68 10.36a.6.6 0 0 0 0 .88l10.68 10.36c.12.12.28.18.45.18.16 0 .32-.06.45-.18a.6.6 0 0 0 0-.88z"/></defs><g><g transform="translate(-158 -684)"><use fill="#040f22" xlinkHref="#tdnaa"/></g></g></svg>
+                            }
                             nextLabel={
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="12" height="22" viewBox="0 0 12 22"><defs><path id="3ngea" d="M495.8 694.54l-10.68-10.36a.65.65 0 0 0-.9 0 .6.6 0 0 0 0 .88l10.22 9.92-10.22 9.92a.6.6 0 0 0 0 .88c.12.12.29.18.45.18.16 0 .33-.06.45-.18l10.68-10.36a.6.6 0 0 0 0-.88z"/></defs><g><g transform="translate(-484 -684)"><use fill="#040f22" xlinkHref="#3ngea"/></g></g></svg>
                             }
@@ -33,8 +36,8 @@ class CalendarList extends Component {
                             prev2Label={null}
                         />
                     </div>
-                    <div className={`col-md`}>
-                        calendar
+                    <div className="col-md calendar-view-events__wrapper">
+                        
                     </div>
                 </div>
             </div>
