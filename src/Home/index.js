4e89a839f83715ddc31e38f6773dccbab281724c
Gergana Mihaylova <geriss1995@gmail.com>
2018-06-06 17:44:51 +0300

    Change: Home component

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,54 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import Loading from '../common/Loading';
-import Event from '../Events/Event';
 import EventsList from '../Events/List';
-import { fetchConferences } from '../Events/List/duck';
-import { fetchWishListIfNeeded } from '../Events/WishList/duck';
 import CalendarList from '../Events/CalendarList';
 import ListViewIcon from '../common/ListViewIcon';
 import CalendarViewIcon from '../common/CalendarViewIcon';
 
 class HomePage extends Component {
     static propTypes = {
-        events: PropTypes.shape({
-            isFetching: PropTypes.bool,
-            lastFetched: PropTypes.number,
-            data: PropTypes.arrayOf(PropTypes.instanceOf(Event)),
-            error: PropTypes.number,
-        }).isRequired,
         auth: PropTypes.shape({
             isAuthenticated: PropTypes.bool,
             token: PropTypes.string,
         }).isRequired,
-        wishList: PropTypes.shape({
-            data: PropTypes.arrayOf(PropTypes.instanceOf(Event)),
-            isFetching: PropTypes.bool,
-            lastFetched: PropTypes.number,
-        }),
-        fetchConferences: PropTypes.func.isRequired,
-        fetchWishListIfNeeded: PropTypes.func.isRequired,
     };
 
-    static defaultProps = {
-        wishList: {},
-    }
 
     constructor(props) {
         super(props);
 
         this.state = {
-            activeView: 'CALENDAR',
+            activeView: 'LIST',
         };
-    }
-
-    componentDidMount() {
-        this.props.fetchConferences(() => {
-            if (this.props.auth.isAuthenticated) {
-                this.props.fetchWishListIfNeeded(this.props.auth.token);
-            }
-        });
     }
 
     toggleListView = () => {
@@ -60,24 +32,7 @@ class HomePage extends Component {
         this.setState({ activeView: 'CALENDAR' });
     }
 
-
     render() {
-        const { error } = this.props.events;
-        const { isFetching } = this.props.events;
-
-        if (error !== null) {
-            return <Loading />;
-        }
-
-        if (isFetching) {
-            return <Loading />;
-        }
-        if (this.props.auth.isAuthenticated) {
-            if (this.props.wishList.isFetching || this.props.wishList.isFetching === null) {
-                return <Loading />;
-            }
-        }
-
         return (
             <div>
                 <div className={`${this.props.auth.isAuthenticated ? 'buttons-toggle-view__wrapper button-toggle-view__wrapper--auth' : 'bg-white'} py-2`}>
@@ -96,10 +51,7 @@ class HomePage extends Component {
                 </div>
 
                 {this.state.activeView === 'LIST' ?
-                    <EventsList
-                        events={this.props.events.data || undefined}
-                        wishList={this.props.wishList.data}
-                    />
+                    <EventsList />
                     :
                     <CalendarList />
                 }
@@ -108,17 +60,8 @@ class HomePage extends Component {
     }
 }
 
-const mapStateToProps = ({ events, auth, wishList }) => {
-    return {
-        events,
-        auth,
-        wishList,
-    };
-};
+const mapStateToProps = ({ auth }) => ({
+    auth,
+});
 
-const mapDispatchToProps = {
-    fetchConferences,
-    fetchWishListIfNeeded,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default connect(mapStateToProps)(HomePage);
