5c3e85da8c5b68e07f78657e37af7c81e7d863ff
gergana95 <gergana@devlabs.bg>
2018-11-12 17:36:22 +0200

    Change: use attendable field from b/e

--- src/common/Card.js
+++ src/common/Card.js
@@ -177,6 +177,7 @@ class Card extends Component {
 
     render() {
         const { event } = this.props;
+
         return (
             <div className="card mb-4 flex-sm-row">
                 {this.renderModal()}
@@ -192,12 +193,12 @@ class Card extends Component {
                     <span className="d-flex justify-content-between">
                         <div className="card__info">
                             <div className="mb-2">
-                                <img src={this.props.past ? calendarPassed : calendar} className="mr-1" alt="small calendar" />
+                                <img src={this.props.past || !event.attendable ? calendarPassed : calendar} className="mr-1" alt="small calendar" />
                                 <span className="card__dates"> {getFormattedDate(event.start, event.end)}
                                 </span>
                             </div>
                             <div className="mt-1">
-                                <img className="mr-1" width="19" height="19" src={this.props.past ? placeHolderPassed : placeHolder} alt="placeholder" />
+                                <img className="mr-1" width="19" height="19" src={this.props.past || !event.attendable ? placeHolderPassed : placeHolder} alt="placeholder" />
                                 <span className="card__dates"> {event.venue}, {event.city}, {event.country}</span>
                             </div>
                         </div>
@@ -211,7 +212,7 @@ class Card extends Component {
                                     className="attend"
                                     id={event.id}
                                     token={this.props.authToken}
-                                    past={this.props.past ? true : false}
+                                    past={this.props.past || !event.attendable ? true : false}
                                     wishListed={this.props.wishListed}
                                     onClick={this.handleAttendClick}
                                     isActive={this.state.isActive}
