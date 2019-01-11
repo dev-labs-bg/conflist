87000582c5e47922e05cede641a08095e22d88a2
gergana95 <gergana@devlabs.bg>
2019-01-11 15:50:35 +0200

    Change: text on feedback page based on rating int

--- src/Feedback/index.js
+++ src/Feedback/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../common/Logo';
+import { connect } from 'react-redux';
 
-const feedback = () => (
+const feedback = ({ rating }) => (
     <div className="container mx-auto pt-5 pb-5">
-        <h1 className="text-center mb-5"> Thank you for your feedback, we hope you enjoyed that conference! </h1>
-
-        <h3 className="font-weight-normal mb-5">
+        {
+            rating === '0' ?
+                (<h1 className="text-center mb-5">
+                  Looks like you didn&apos;t attend that conference.
+                </h1>) :
+                (<h1 className="text-center mb-5">
+                  Thank you for your feedback, we hope you enjoyed that conference!
+                </h1>
+                )
+        }
+        <h3 className="font-weight-normal mb-4">
             You can find more conferences
-            at <Link href="#home" to="/home">
-                <Logo authentication />
+            at <Link className="text-info" to="/home">
+                conflist
                </Link>,
         </h3>
-        <h3> Or <Link className="text-info" to="/conference-suggest">
-            Suggest a conference
+        <h3> or <Link className="text-info" to="/conference-suggest">
+            suggest a conference
         </Link> to improve our list!
         </h3>
 
     </div>
 );
 
-export default feedback;
+const mapStateToProps = ({}, { location }) => {
+    const pathnameArray = location.search.split('=');
+    return {
+        rating: pathnameArray[1],
+    };
+};
+
+export default connect(mapStateToProps)(feedback);
